127d59bc974f76e65364adbe93f51bfdac354fdc
Tom Beckmann <tomjonabc@gmail.com>
2016-10-21 13:44:13 +0200

    hbft: fix wrong interpretation of codepoint in hb
    after shaping, codepoints in hb_glyph_info_t no longer correspond to
    unicode values, but to indices into the font's charmap. instead, we can
    use the hb_glyph_info_t's cluster index to access the character in the
    original text string

--- HbFtPlugin/unix/Fonts.c
+++ HbFtPlugin/unix/Fonts.c
@@ -32,6 +32,10 @@ static FT_Error face_requester(FTC_FaceID face_id,
 
 font_library_t *font_library_new() {
 	font_library_t *l = (font_library_t *) malloc(sizeof(font_library_t));
+	l->n_fonts = 0;
+	l->max_fonts = 0;
+	l->fonts = NULL;
+
 	if (FT_Init_FreeType(&l->ftlib))
 		goto clean;
 
@@ -71,7 +75,7 @@ void font_library_free(font_library_t *l) {
 font_t *font_load(font_library_t *lib, const char *file) {
 	if (lib->max_fonts == 0) {
 		lib->max_fonts = 16;
-		lib->fonts = calloc(sizeof(font_t), lib->n_fonts);
+		lib->fonts = calloc(sizeof(font_t), lib->max_fonts);
 	} else if (lib->n_fonts >= lib->max_fonts) {
 		lib->max_fonts *= 2;
 		lib->fonts = realloc(lib->fonts, sizeof(font_t) * lib->max_fonts);
@@ -214,9 +218,8 @@ static FT_Error renderIndex(font_library_t *lib, FTC_ScalerRec *scaler, FT_ULong
 	return FT_Err_Ok;
 }
 
-static double _width_of_next_word(hb_glyph_info_t *first, hb_glyph_info_t *last, hb_glyph_position_t *p, int *word_length) {
+static double _width_of_next_word(hb_glyph_info_t *first, hb_glyph_info_t *last, hb_glyph_position_t *p, const char *text, int *word_length) {
 #define UTF8_IS_WHITESPACE(x) (\
-	x == 0x0003 ||					/* END OF TEXT, used by harfbuzz */\
 	(x >= 0x0009 && x <= 0x000D) ||	/* White_Space # Cc	<control-0009>..<control-000D> */\
 	x == 0x0020 ||					/* White_Space # Zs	SPACE */                         \
 	x == 0x0085 ||         			/* White_Space # Cc	<control-0085> */                \
@@ -231,7 +234,7 @@ static double _width_of_next_word(hb_glyph_info_t *first, hb_glyph_info_t *last,
 
 	double distance = 0.0;
 	*word_length = 0;
-	while (first != last && !UTF8_IS_WHITESPACE(first->codepoint)) {
+	while (first != last && !UTF8_IS_WHITESPACE(text[first->cluster])) {
 		distance += p->x_advance / 64.0;
 		p++;
 		(*word_length)++;
@@ -241,12 +244,21 @@ static double _width_of_next_word(hb_glyph_info_t *first, hb_glyph_info_t *last,
 }
 
 FT_Error surface_render_text(surface_t *surface, font_library_t *lib, font_t *f, int ptSize, int dpi, const char *text) {
-	unsigned int glyph_count;
-	hb_glyph_info_t *glyph_infos;
-	hb_glyph_position_t *glyph_positions;
+	static const hb_feature_t hb_features[] = {
+		{ HB_TAG('k','e','r','n'), 1, 0, -1  },
+		{ HB_TAG('c','l','i','g'), 1, 0, -1  },
+		{ HB_TAG('l','i','g','a'), 1, 0, -1  }
+	};
+
+	unsigned int glyph_count, maxHeight;
+	int remainingLetters;
+	hb_glyph_info_t *currentGlyphInfo, *lastGlyphInfo;
+	hb_glyph_position_t *currentGlyphPos;
+	hb_font_t *hb_font;
 	FT_Face ft_face;
 	FT_Size size;
 	FT_Error error;
+	double x, y;
 
 	lib->scaler.face_id = f;
 	lib->scaler.width  = (FT_UInt) ptSize * 64;
@@ -260,32 +272,26 @@ FT_Error surface_render_text(surface_t *surface, font_library_t *lib, font_t *f,
 	if ((error = FTC_Manager_LookupSize(lib->ftmanager, &lib->scaler, &size)))
 		return error;
 
-	hb_font_t *hb_font = hb_ft_font_create(ft_face, NULL);
+	hb_font = hb_ft_font_create(ft_face, NULL);
 	hb_buffer_clear_contents(lib->hb_buffer);
 	hb_buffer_set_content_type(lib->hb_buffer, HB_BUFFER_CONTENT_TYPE_UNICODE);
 	hb_buffer_add_utf8(lib->hb_buffer, text, strlen(text), 0, strlen(text));
 	hb_buffer_set_script(lib->hb_buffer, HB_SCRIPT_LATIN);
 	hb_buffer_guess_segment_properties(lib->hb_buffer);
 
-	hb_shape(hb_font, lib->hb_buffer, NULL, 0);
-	glyph_infos = hb_buffer_get_glyph_infos(lib->hb_buffer, &glyph_count);
-	glyph_positions = hb_buffer_get_glyph_positions(lib->hb_buffer, NULL);
+	hb_shape(hb_font, lib->hb_buffer, hb_features, sizeof(hb_features) / sizeof(hb_feature_t));
 
-	int maxHeight = (int) ((ft_face->size->metrics.ascender - ft_face->size->metrics.descender) / 64.0);
-	int remaining_letters = 0;
-	hb_glyph_position_t *currentGlyphPos = glyph_positions;
-	hb_glyph_info_t *currentGlyphInfo = glyph_infos;
-	hb_glyph_info_t *lastGlyphInfo = glyph_infos + glyph_count;
-	double x = 0.0;
-	double y = ft_face->size->metrics.ascender / 64.0;
-	const char *cur = text;
+	currentGlyphPos = hb_buffer_get_glyph_positions(lib->hb_buffer, &glyph_count);
+	currentGlyphInfo = hb_buffer_get_glyph_infos(lib->hb_buffer, NULL);
+	lastGlyphInfo = currentGlyphInfo + glyph_count;
+
+	maxHeight = (int) ((ft_face->size->metrics.ascender - ft_face->size->metrics.descender) / 64.0);
+	remainingLetters = 0;
+	x = 0.0;
+	y = ft_face->size->metrics.ascender / 64.0;
 	for (unsigned int i = 0; i < glyph_count; i++) {
-		hb_codepoint_t codepoint = glyph_infos[i].codepoint;
-		// FT_UInt index = FTC_CMapCache_Lookup(lib->ftcmap_cache, (FTC_FaceID) f, f->cmap_index, codepoint);
-
-		cur++;
-		if (remaining_letters < 0) {
-			int width = _width_of_next_word(currentGlyphInfo, lastGlyphInfo, currentGlyphPos, &remaining_letters);
+		if (remainingLetters < 0) {
+			int width = _width_of_next_word(currentGlyphInfo, lastGlyphInfo, currentGlyphPos, text, &remainingLetters);
 			if (width < surface->w && x + width > surface->w) {
 				x = 0;
 				y += maxHeight;
@@ -295,7 +301,7 @@ FT_Error surface_render_text(surface_t *surface, font_library_t *lib, font_t *f,
 		FT_Glyph glyph;
 		int bitmap_left, bitmap_top;
 		surface_t bitmap;
-		renderIndex(lib, &lib->scaler, codepoint, &glyph, &bitmap, &bitmap_left, &bitmap_top);
+		renderIndex(lib, &lib->scaler, currentGlyphInfo->codepoint, &glyph, &bitmap, &bitmap_left, &bitmap_top);
 
 		if (x + currentGlyphPos->x_advance / 64.0 > surface->w) {
 			x = 0;
@@ -305,12 +311,13 @@ FT_Error surface_render_text(surface_t *surface, font_library_t *lib, font_t *f,
 		if (y + currentGlyphPos->y_advance / 64.0 > surface->h)
 			return FT_Err_Raster_Overflow;
 
-		if (bitmap.data)
+		if (bitmap.data) {
 			bitblt(&bitmap,
 					surface,
 					x + currentGlyphPos->x_offset / 64.0 + bitmap_left,
 					y - currentGlyphPos->y_offset / 64.0 - bitmap_top,
 					&blendColor);
+		}
 
 		if (glyph)
 			FT_Done_Glyph(glyph);
@@ -319,9 +326,7 @@ FT_Error surface_render_text(surface_t *surface, font_library_t *lib, font_t *f,
 		y -= currentGlyphPos->y_advance / 64.0;
 		currentGlyphPos++;
 		currentGlyphInfo++;
-		remaining_letters--;
-
-		assert(x < surface->w && y < surface->h);
+		remainingLetters--;
 	}
 
 	hb_font_destroy(hb_font);
