c0b6989b19fd39830ea20f1a160c8ee7daf6be95
Justus Hildebrand <justus.hildebrand@student.hpi.de>
2016-10-24 12:29:05 +0200

    fixed test regarding restructuring of our PList class

--- ampfel/ampelScript.sh
+++ ampfel/ampelScript.sh
@@ -8,18 +8,20 @@ while true
 do 
 
 
-NEW_STATE=$(curl -s -LH "Accept: application/vnd.travis-ci.2+json" "https://api.travis-ci.org/repos/HPI-SWA-Teaching/PrettyPrettyPrint/builds" | grep -o '"state":.[a-z\"]*' | head -n 1);
+NEW_STATE=$(curl -s -LH "Accept: application/vnd.travis-ci.2+json" "https://api.travis-ci.org/repos/HPI-SWA-Lab/BP2016H1/builds" | grep -o '"state":.[a-z\"]*' | head -n 1);
 
 if [ "$CURRENT_STATE" != "$NEW_STATE" ];
 then
 	CURRENT_STATE=$NEW_STATE;
 	if [ $CURRENT_STATE = "\"state\":\"errored\"" ]; then
 		sispmctl -o 2 -f 1
-	else
+	elif [ $CURRENT_STATE = "\"state\":\"passed\"" ]; then
 		sispmctl -o 1 -f 2
 
+	else
+		sispmctl -f 1 -f 2
 	fi
 fi
 
-sleep 1m
+sleep 30s
 done
