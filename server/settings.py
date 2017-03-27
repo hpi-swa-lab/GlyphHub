72e8be8e62356be28b7e8ae1c480631ee23fec0b
Tom Beckmann <tomjonabc@gmail.com>
2017-03-27 17:34:00 +0200

    Register missing models to REST interface

--- server/settings.py
+++ server/settings.py
@@ -1,4 +1,4 @@
-from tables import User
+from tables import *
 from eve.utils import config
 
 ID_FIELD = '_id'
@@ -14,5 +14,14 @@ IF_MATCH = False
 HATEOAS = False
 
 DOMAIN = {
-    'user': User._eve_schema['user']
+    'user': User._eve_schema['user'],
+    'tag': Tag._eve_schema['tag'],
+    'sample_text': SampleText._eve_schema['sample_text'],
+    'family': Family._eve_schema['family'],
+    'font': Font._eve_schema['font'],
+    'glyph': Glyph._eve_schema['glyph'],
+    'thread': Thread._eve_schema['thread'],
+    'codepoint': Codepoint._eve_schema['codepoint'],
+    'comment': Comment._eve_schema['comment'],
+    'attachment': Attachment._eve_schema['attachment']
 }
