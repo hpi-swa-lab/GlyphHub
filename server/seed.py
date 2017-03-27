cda7e0d2242e5cc3dafca63a3af01f150fcd37be
Tom Beckmann <tomjonabc@gmail.com>
2017-03-27 17:28:42 +0200

    Fix seeds for new names

--- server/seed.py
+++ server/seed.py
@@ -1,23 +1,23 @@
 from tables import *
 
-fira = Font(name='Fira Sans Regular', family_id=1, author_id=1)
+fira = Font(fontName='Fira Sans Regular', family_id=1, author_id=1)
 fira.tags.append(Tag(text='#pretty', type='opinion'))
 fira.tags.append(Tag(text='Latin', type='language'))
 
 thread1 = Thread(title='I don\'t like this word')
-thread1.glyphs.append(Glyph(name='A', version_hash='9c7075ca420f30aedb27c48102466313fa4d12c8', font_id=1))
-thread1.glyphs.append(Glyph(name='a', version_hash='9c7075ca420f30aedb27c48102466313fa4d12c8', font_id=1))
-thread1.glyphs.append(Glyph(name='s', version_hash='9c7075ca420f30aedb27c48102466313fa4d12c8', font_id=1))
+thread1.glyphs.append(Glyph(glyphName='A', version_hash='9c7075ca420f30aedb27c48102466313fa4d12c8', font_id=1))
+thread1.glyphs.append(Glyph(glyphName='a', version_hash='9c7075ca420f30aedb27c48102466313fa4d12c8', font_id=1))
+thread1.glyphs.append(Glyph(glyphName='s', version_hash='9c7075ca420f30aedb27c48102466313fa4d12c8', font_id=1))
 
 entities = [
-    User(name='Eva', password='eveisevil'),
-    User(name='Tom', password='safepwissafe'),
-    Family(name='Fira'),
+    User(userName='Eva', password='eveisevil'),
+    User(userName='Tom', password='safepwissafe'),
+    Family(familyName='Fira'),
     fira,
     thread1,
-    Codepoint(value=0x0041, size=12.5, features='liga', thread_id=1, font_id=1),
-    Codepoint(value=0x0061, size=12.5, features='liga', thread_id=1, font_id=1),
-    Codepoint(value=0x0073, size=12.5, features='liga', thread_id=1, font_id=1),
+    Codepoint(unicodeValue=0x0041, pointSize=12.5, features='liga', thread_id=1, font_id=1),
+    Codepoint(unicodeValue=0x0061, pointSize=12.5, features='liga', thread_id=1, font_id=1),
+    Codepoint(unicodeValue=0x0073, pointSize=12.5, features='liga', thread_id=1, font_id=1),
     Comment(text='why would anyone comment on aas', author_id=2, thread_id=1),
     Comment(text='because.', author_id=1, thread_id=1),
     SampleText(title='Evil Wizards', text='Mad wizards brew evil jack with horses', author_id=2)
