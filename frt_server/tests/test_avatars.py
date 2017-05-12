ff6412a0d0424479730090911fffe89933a9e737
Tom Beckmann <tomjonabc@gmail.com>
2017-05-12 11:47:43 +0200

    i #210 update timestamp when uploading a new avatar

---
+++ frt_server/tests/test_avatars.py
@@ -0,0 +1,25 @@
+from frt_server.tests import TestMinimal
+from frt_server.tables import User
+
+from sqlalchemy.orm import joinedload
+
+import os
+
+class UploadTestCase(TestMinimal):
+    def setUp(self):
+        super(UploadTestCase, self).setUp()
+        self.login_as('Eva', 'eveisevil')
+
+        self.session = self.connection.session
+        self.user = self.session.query(User).one()
+
+    def test_upload_works(self):
+        data, status = self.upload_file('/user_avatar/upload', 'file', 'assets/penguin.png')
+        self.assertEqual(status, 200)
+
+    def test_update_at_is_updated(self):
+        current = self.user.updated_at
+        data, status = self.upload_file('/user_avatar/upload', 'file', 'assets/penguin.png')
+        self.assertEqual(status, 200)
+        new = self.session.query(User).one().updated_at
+        self.assertGreater(new, current)
