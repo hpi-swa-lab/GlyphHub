5ce2d522919f3290f29338470e4bacf03e44e018
Alexander Löser <alexander.loeser@student.hpi.de>
2017-05-22 15:56:45 +0200

    i #208 add script to reset database

--- db-backup.sh
+++ db-backup.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
-
 set -e
 
-folder="backups"
+FILE=$1
+if [ ! -f "$FILE" ]; then
+  echo "$FILE does not exist"
+  exit 1
+fi
+
+read -p "Create a database backup before resetting? (y/n) "
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "creating backup.."
+  /bin/bash db-backup.sh "$HOME/emergency_backups"
+fi
+
+root_mysql_pw=$(sudo cat /root/.mysql_root_pw)
+mysql -u root -p"$root_mysql_pw" < create_database.sql
+
 password=$(sed -rn 's|.*mysql.*://.+:(.+)\@.*|\1|p' config/config.py)
-filename="backup-$(date +%d-%m-%Y).sql.gz"
-mkdir -p $folder
-mysqldump -u frt -p"$password" frt_data | gzip > "$folder/$filename"
+gzip -cd "$FILE" | mysql -u frt -p"$password" frt_data
